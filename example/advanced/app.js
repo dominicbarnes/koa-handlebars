5856ef70e672bec5316c96e457973f56de2c3188
Dominic Barnes <dominic@segment.io>
2014-09-18 16:19:03 -0700

    initial commit

---
+++ example/advanced/app.js
@@ -0,0 +1,22 @@
+var koa = require("koa");
+var handlebars = require("./handlebars.js");
+
+var app = koa();
+
+app.use(require("koa-favi")());
+app.use(require("koa-logger")());
+
+app.use(handlebars({
+  cache: app.env !== "development",
+  defaultLayout: "main",
+  viewPath: "pages",
+  helpers: require("./lib/helpers.js")
+}).middleware());
+
+app.use(function *() {
+  yield this.render("test", {
+    user: { name: "World" }
+  });
+});
+
+app.listen(3000);
